453e3e31e5446ebd8c15a5c48a530a4be2696308
Ryan Gong <ryangongcellphone@gmail.com>
2024-06-25 11:14:58 -0400

    Add files via upload

---
+++ CTY Python Day 2/Python CTY/alarm.py
@@ -0,0 +1,24 @@
+# Ryan's Python Project At CTY - 6/24/24
+'''
+print("Hello world")
+
+x = input("Enter an integer: ")
+y = input("Enter a second integer: ")
+
+if x > y:
+    print(x + " is greater than " + y)
+elif x < y:
+    print(x + " is less than " + y)
+elif x == y:
+    print(x + " is equal to " + y)
+else:
+    print("Invalid")
+'''
+
+
+
+# Python Mini Project
+name = input("Hello! Please enter your name: ")
+age = input("Enter your age: ")
+month = input("Enter the month you were born in: ")
+print("Hi " + name + ", nice to meet you! You are " + age + " years old. Born in " + month + "." )
