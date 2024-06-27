fcdf5799ec068663b367c59bec5d5d13c3e59e2d
Ryan Gong <ryangongcellphone@gmail.com>
2024-06-26 20:32:36 -0400

    Add files via upload

---
+++ CTY Python Day 4/list.py
@@ -0,0 +1,16 @@
+print("Hello world")
+
+songs = ["song1", "song2", "song3", "song4"]
+
+print(songs)
+print(songs[0]) #indexing a list
+
+
+i=0
+for i in range(0,100):
+    print(i)
+
+
+#userInput = 2
+#for x = 10; x < userInput; x+1:
+#    print(x)
